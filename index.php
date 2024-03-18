7c87eb15c179b09b41e3fc2ec22b7e5de0b586ed
Tatum Baldwin <tjbaldwin@students.nic.edu>
2024-03-18 13:44:27 -0700

    testing

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 11.1: HTML Table with Database Records</title>
+</head>
+<body>
+    
+</body>
+</html>
